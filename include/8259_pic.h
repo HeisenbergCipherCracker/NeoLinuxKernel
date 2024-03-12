9cb928ffbdf73c378ed67dd85a311f423428fb2f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-12 11:54:31 -0400

    Updated include files

---
+++ include/8259_pic.h
@@ -0,0 +1,28 @@
+#ifndef _8259_PIC_H
+#define _8259_PIC_H
+
+#include "types.h"
+
+#define PIC1 0x20 /*Io base address for MAster PIC*/
+#define PIC2 0xA0  /* IO base address for slave PIC */
+#define PIC_COMMAND PIC1
+#define PIC1_DATA (PIC1+1)    /* master data */
+#define PIC2_COMMAND PIC2
+#define PIC2_DATA (PIC2+1)    /* slave data */
+
+#define PIC_EOI 0x20   /* end of interrupt */
+
+#define ICW1 0x11    /* interrupt control command word PIC for initialization */
+#define ICW4_8086 0x01    /* 8086/88 (MCS-80/85) mode */
+
+/**
+ * initialize 8259 PIC with default IRQ's defined in isr.h
+ */
+void pic8259_init();
+
+/**
+ * send end of interrupt command to PIC 8259
+ */
+void pic8259_eoi(uint8 irq);
+
+#endif // _8259_PIC_H
