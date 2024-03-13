fa3b38f9f2b190582dd6401babbc7317c82baaeb
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-12 23:09:37 -0400

    Update for assembly code docs

--- src/asm/entry.asm
+++ src/asm/entry.asm
@@ -1,7 +1,3 @@
-/*
-*Neo Linux: A cutting-edge kernel for
-*next-generation computing, released under the GPL 3.0 license. READ LICENSE file for more information
-*/
 ; constants for multiboot header
 MEMINFO     equ  1<<0
 BOOTDEVICE  equ  1<<1
--- src/asm/load_gdt.asm
+++ src/asm/load_gdt.asm
@@ -1,7 +1,3 @@
-/*
-*Neo Linux: A cutting-edge kernel for
-*next-generation computing, released under the GPL 3.0 license. READ LICENSE file for more information
-*/
 section .text
     global load_gdt
 
