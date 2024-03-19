4eabc29dbd9450038875ce5f2f7bb776f583ff71
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-18 21:51:45 -0400

    Added some minor updates for macros in PIC

--- scripts/eoi/pic.c
+++ scripts/eoi/pic.c
@@ -1,147 +1,177 @@
-#include "pics.h"
+#include "pics.h" // Include custom PIC definitions header
 #include <stdio.h>
 #include <stdint.h>
+
 /*
-* This would define the 8259 PIC interrupts
-*/
-#define PIC_EOI		0x20		/* End-of-interrupt command code */
- 
-void PIC_sendEOI(uint8_t irq)
-{
-	if(irq >= 8)
-		outb(PIC2_COMMAND,PIC_EOI);
- 
-	outb(PIC1_COMMAND,PIC_EOI);
-}
-///////////////////////////////////////////////////////////////////////////////////////////
-/*!
-@brief Initializes the programmable interrupt controller
-- @ref https://wiki.osdev.org/PIC
-*/
-/* reinitialize the PIC controllers, giving them specified vector offsets
-   rather than 8h and 70h, as configured by default */
- 
-#define ICW1_ICW4	0x01		/* Indicates that ICW4 will be present */
-#define ICW1_SINGLE	0x02		/* Single (cascade) mode */
-#define ICW1_INTERVAL4	0x04		/* Call address interval 4 (8) */
-#define ICW1_LEVEL	0x08		/* Level triggered (edge) mode */
-#define ICW1_INIT	0x10		/* Initialization - required! */
- 
-#define ICW4_8086	0x01		/* 8086/88 (MCS-80/85) mode */
-#define ICW4_AUTO	0x02		/* Auto (normal) EOI */
-#define ICW4_BUF_SLAVE	0x08		/* Buffered mode/slave */
-#define ICW4_BUF_MASTER	0x0C		/* Buffered mode/master */
-#define ICW4_SFNM	0x10		/* Special fully nested (not) */
- 
+ * This section contains functions and definitions related to the 8259 Programmable Interrupt Controller (PIC).
+ */
+
+// Define the End-of-Interrupt command code
+#define PIC_EOI 0x20
+#define ICW4_8086 0x01    /* 8086/88 (MCS-80/85) mode */
+
+
+// Define port addresses and commands for PICs
+#define PIC1 0x20 // IO base address for master PIC
+#define PIC2 0xA0 // IO base address for slave PIC
+#define ICW1_INIT 0x10       /* Initialization - required! */
+#define ICW1_ICW4 0x01       /* ICW4 (not) needed */
+#define PIC1_COMMAND PIC1
+#define PIC1_DATA (PIC1 + 1)
+#define PIC2_COMMAND PIC2
+#define PIC2_DATA (PIC2 + 1)
+
+// Function prototypes for low-level I/O operations
+void outb(uint16_t port, uint8_t value);
+uint8_t inb(uint16_t port);
+void io_wait();
+
 /*
-arguments:
-	offset1 - vector offset for master PIC
-		vectors on the master become offset1..offset1+7
-	offset2 - same for slave PIC: offset2..offset2+7
-*/
-void PIC_remap(int offset1, int offset2)
-{
-	uint8_t a1, a2;
- 
-	a1 = inb(PIC1_DATA);                        // save masks
-	a2 = inb(PIC2_DATA);
- 
-	outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
-	io_wait();
-	outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
-	io_wait();
-	outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
-	io_wait();
-	outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
-	io_wait();
-	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
-	io_wait();
-	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
-	io_wait();
- 
-	outb(PIC1_DATA, ICW4_8086);               // ICW4: have the PICs use 8086 mode (and not 8080 mode)
-	io_wait();
-	outb(PIC2_DATA, ICW4_8086);
-	io_wait();
- 
-	outb(PIC1_DATA, a1);   // restore saved masks.
-	outb(PIC2_DATA, a2);
+ * Function: outb
+ * ----------------------------
+ * Sends a byte to a specified I/O port.
+ *
+ * port: The I/O port address.
+ * value: The value to be written.
+ */
+void outb(uint16_t port, uint8_t value) {
+    asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
 }
 
 /*
-Disable  all interrupts from both PICs.
-- @ref  pic_disable() disables only one IRQ
-*/
+ * Function: inb
+ * ----------------------------
+ * Reads a byte from a specified I/O port.
+ *
+ * port: The I/O port address.
+ *
+ * returns: The value read from the port.
+ */
+uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
 
+/*
+ * Function: io_wait
+ * ----------------------------
+ * Waits for an I/O operation to complete.
+ * This is achieved by sending a dummy read to an unused I/O port.
+ */
+void io_wait() {
+    asm volatile ("outb %%al, $0x80" : : "a"(0));
+}
+
+/*
+ * Function: PIC_sendEOI
+ * ----------------------------
+ * Sends the End-of-Interrupt (EOI) command to the PIC(s) for the specified IRQ.
+ *
+ * irq: The IRQ for which EOI is to be sent.
+ */
+void PIC_sendEOI(uint8_t irq) {
+    if (irq >= 8)
+        outb(PIC2_COMMAND, PIC_EOI); // Send EOI to slave PIC if necessary
+    outb(PIC1_COMMAND, PIC_EOI); // Send EOI to master PIC
+}
+
+/*
+ * Function: PIC_remap
+ * ----------------------------
+ * Initializes the PIC by remapping its vectors.
+ *
+ * offset1: Vector offset for master PIC.
+ * offset2: Vector offset for slave PIC.
+ */
+void PIC_remap(int offset1, int offset2) {
+    uint8_t a1, a2;
+ 
+    // Save current masks
+    a1 = inb(PIC1_DATA);
+    a2 = inb(PIC2_DATA);
+ 
+    // Start initialization sequence
+    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+ 
+    // Set vector offsets
+    outb(PIC1_DATA, offset1);
+    io_wait();
+    outb(PIC2_DATA, offset2);
+    io_wait();
+ 
+    // Configure PIC cascading
+    outb(PIC1_DATA, 4); // Tell master PIC about slave PIC at IRQ2
+    io_wait();
+    outb(PIC2_DATA, 2); // Tell slave PIC its cascade identity
+    io_wait();
+ 
+    // Configure PIC mode (8086 mode)
+    outb(PIC1_DATA, ICW4_8086);
+    io_wait();
+    outb(PIC2_DATA, ICW4_8086);
+    io_wait();
+ 
+    // Restore saved masks
+    outb(PIC1_DATA, a1);
+    outb(PIC2_DATA, a2);
+}
+
+/*
+ * Function: pic_disable
+ * ----------------------------
+ * Disables all interrupts from both PICs.
+ */
 void pic_disable(void) {
-    outb(PIC1_DATA, 0xff);
-    outb(PIC2_DATA, 0xff);
+    outb(PIC1_DATA, 0xFF); // Mask all interrupts on master PIC
+    outb(PIC2_DATA, 0xFF); // Mask all interrupts on slave PIC
 }
 
 /*
-
-In a PIC (Programmable Interrupt Controller), 
-there's a register called the IMR (Interrupt Mask Register), 
-which is 8 bits wide. This register acts like a bitmap, 
-where each bit represents a request line going into the PIC. 
-When a bit is set in the IMR, it means the PIC will ignore any interrupts coming from the corresponding request line and continue its normal operation. It's important to note that setting the mask on a higher request line won't affect a lower line. For example, masking IRQ2 will cause the Slave PIC to stop raising IRQs, but it won't affect IRQs from lower-priority lines.
-*/
-
-/*
-The PIC has an internal register called the IMR, or the Interrupt Mask Register. It is 8 bits wide. This register is a bitmap of the request lines going into the PIC. When a bit is set, the PIC ignores the request and continues normal operation. Note that setting the mask on a higher request line will not affect a lower line. Masking IRQ2 will cause the Slave PIC to stop raising IRQs.*/
-
+ * Function: IRQ_set_mask
+ * ----------------------------
+ * Sets the interrupt mask for the specified IRQ line.
+ *
+ * IRQline: The IRQ line to set the mask for.
+ */
 void IRQ_set_mask(uint8_t IRQline) {
     uint16_t port;
     uint8_t value;
  
-    if(IRQline < 8) {
+    if (IRQline < 8) {
         port = PIC1_DATA;
     } else {
         port = PIC2_DATA;
         IRQline -= 8;
     }
+ 
+    // Set the mask bit for the specified IRQ line
     value = inb(port) | (1 << IRQline);
-    outb(port, value);        
-}
- 
-void IRQ_clear_mask(uint8_t IRQline) {
-    uint16_t port;
-    uint8_t value;
- 
-    if(IRQline < 8) {
-        port = PIC1_DATA;
-    } else {
-        port = PIC2_DATA;
-        IRQline -= 8;
-    }
-    value = inb(port) & ~(1 << IRQline);
     outb(port, value);        
 }
 
-void IRQ_set_mask(uint8_t IRQline) {
-    uint16_t port;
-    uint8_t value;
- 
-    if(IRQline < 8) {
-        port = PIC1_DATA;
-    } else {
-        port = PIC2_DATA;
-        IRQline -= 8;
-    }
-    value = inb(port) | (1 << IRQline);
-    outb(port, value);        
-}
- 
+/*
+ * Function: IRQ_clear_mask
+ * ----------------------------
+ * Clears the interrupt mask for the specified IRQ line.
+ *
+ * IRQline: The IRQ line to clear the mask for.
+ */
 void IRQ_clear_mask(uint8_t IRQline) {
     uint16_t port;
     uint8_t value;
  
-    if(IRQline < 8) {
+    if (IRQline < 8) {
         port = PIC1_DATA;
     } else {
         port = PIC2_DATA;
         IRQline -= 8;
     }
+ 
+    // Clear the mask bit for the specified IRQ line
     value = inb(port) & ~(1 << IRQline);
     outb(port, value);        
 }
