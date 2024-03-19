0124b2f075efe97a29e8d80e8df50bc14ace5df8
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-18 21:42:15 -0400

    Minor update

--- scripts/eoi/pic.c
+++ scripts/eoi/pic.c
@@ -117,3 +117,31 @@ void IRQ_clear_mask(uint8_t IRQline) {
     value = inb(port) & ~(1 << IRQline);
     outb(port, value);        
 }
+
+void IRQ_set_mask(uint8_t IRQline) {
+    uint16_t port;
+    uint8_t value;
+ 
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) | (1 << IRQline);
+    outb(port, value);        
+}
+ 
+void IRQ_clear_mask(uint8_t IRQline) {
+    uint16_t port;
+    uint8_t value;
+ 
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) & ~(1 << IRQline);
+    outb(port, value);        
+}
