3c8c872871ebf0a857ee56e8425159909633a823
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-18 21:41:04 -0400

    Added more signals for PIC

--- scripts/eoi/pic.c
+++ scripts/eoi/pic.c
@@ -67,3 +67,53 @@ void PIC_remap(int offset1, int offset2)
 	outb(PIC1_DATA, a1);   // restore saved masks.
 	outb(PIC2_DATA, a2);
 }
+
+/*
+Disable  all interrupts from both PICs.
+- @ref  pic_disable() disables only one IRQ
+*/
+
+void pic_disable(void) {
+    outb(PIC1_DATA, 0xff);
+    outb(PIC2_DATA, 0xff);
+}
+
+/*
+
+In a PIC (Programmable Interrupt Controller), 
+there's a register called the IMR (Interrupt Mask Register), 
+which is 8 bits wide. This register acts like a bitmap, 
+where each bit represents a request line going into the PIC. 
+When a bit is set in the IMR, it means the PIC will ignore any interrupts coming from the corresponding request line and continue its normal operation. It's important to note that setting the mask on a higher request line won't affect a lower line. For example, masking IRQ2 will cause the Slave PIC to stop raising IRQs, but it won't affect IRQs from lower-priority lines.
+*/
+
+/*
+The PIC has an internal register called the IMR, or the Interrupt Mask Register. It is 8 bits wide. This register is a bitmap of the request lines going into the PIC. When a bit is set, the PIC ignores the request and continues normal operation. Note that setting the mask on a higher request line will not affect a lower line. Masking IRQ2 will cause the Slave PIC to stop raising IRQs.*/
+
+void IRQ_set_mask(uint8_t IRQline) {
+    uint16_t port;
+    uint8_t value;
+ 
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) | (1 << IRQline);
+    outb(port, value);        
+}
+ 
+void IRQ_clear_mask(uint8_t IRQline) {
+    uint16_t port;
+    uint8_t value;
+ 
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) & ~(1 << IRQline);
+    outb(port, value);        
+}
